56b4ffb12bf01247fce2103254f1d42be32dd566
Kamlesh <kamleshotari.com>
2015-11-10 15:30:47 +0530

    file 10 11

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,16 +1,15 @@
 class StoreController < ApplicationController
   skip_before_filter :authorize
 
-   def index
-  @products=Product.all
-@cart = current_cart
+  def index
+    @products=Product.all
+    @cart = current_cart
   end
-def show
-@products=Product.all
-
-respond_to do |format|
-      format.html {redirect_to(store_index_url)}
-      format.xml  { render :xml => @store }
-end
-end
+  def show
+    @products=Product.all
+    respond_to do |format|
+          format.html {redirect_to(store_index_url)}
+          format.xml  { render :xml => @store }
+    end
+  end
 end
